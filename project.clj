2ae4ef358187a84d559bffdcc566039dfab8ffee
Joe Gallo <joegallo@gmail.com>
2014-10-27 12:49:28 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -17,13 +17,13 @@
   :url "http://github.com/sonian/apodini"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[cheshire "5.1.1"]
+  :dependencies [[cheshire "5.3.1"]
                  [clj-http "0.7.2"]
-                 [commons-codec "1.7"]
+                 [commons-codec "1.9"]
                  [slingshot "0.10.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :plugins [[lein-bikeshed "0.1.0"]]
-  :aliases {"all" ["with-profile" "dev,1.4:dev"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :plugins [[lein-bikeshed "0.1.8"]]
+  :aliases {"all" ["with-profile" "dev,1.5:dev"]
             "test!" ["do" "clean," "deps," "bikeshed," "test"]}
   :min-lein-version "2.0.0")
